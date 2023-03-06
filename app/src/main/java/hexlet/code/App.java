69d483a01fa0e2f6785197d38d0e7aca9b3832d3
step203 <stet169500@gmail.com>
2023-03-06 05:51:04 +0300

    chekstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,20 +2,27 @@ package hexlet.code;
 
 import hexlet.code.games.*;
 
-import static hexlet.code.Engine.hello;
+//import static hexlet.code.Engine.hello;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n" +
-                "1 - Greet\n" +
-                "2 - Even\n" +
-                "3 - Calc\n" +
-                "4 - GCD\n" +
-                "5 - Progression\n" +
-                "6 - Prime\n" +
+        System.out.println("Please enter the game number and press Enter.\n"
+                +
+                "1 - Greet\n"
+                +
+                "2 - Even\n"
+                +
+                "3 - Calc\n"
+                +
+                "4 - GCD\n"
+                +
+                "5 - Progression\n"
+                +
+                "6 - Prime\n"
+                +
                 "0 - Exit");
         System.out.print(Engine.ANSWER);
         int nameInt = scanner.nextInt();
